a2c3ece89f04e01c6ead40b6ca6aad9f8cd3d92f
Quentin DUTILLEUL <quentin.dutilleul@exterieurstock.fr>
2024-07-10 17:19:08 +0200

    Oopsie 3

--- elements/community_store_alma_checkout/dashboard_form.php
+++ elements/community_store_alma_checkout/dashboard_form.php
@@ -21,28 +21,28 @@ use Concrete\Core\Support\Facade\Url;
 </div>
 
 <div class="form-group">
-    <?= $form->label("almaCheckoutTestPublicApiKey"), t("Test Public API Key") ?>
+    <?= $form->label("almaCheckoutTestPublicApiKey", t("Test Public API Key")) ?>
     <?= $form->text("almaCheckoutTestPublicApiKey", $almaCheckoutTestPublicApiKey) ?>
 </div>
 
 <div class="form-group">
-    <?= $form->label("almaCheckoutTestPrivateApiKey"), t("Test Private API Key") ?>
+    <?= $form->label("almaCheckoutTestPrivateApiKey", t("Test Private API Key")) ?>
     <?= $form->text("almaCheckoutTestPrivateApiKey", $almaCheckoutTestPrivateApiKey) ?>
 </div>
 
 <div class="form-group">
-    <?= $form->label("almaCheckoutLivePublicApiKey"), t("Live API Key") ?>
+    <?= $form->label("almaCheckoutLivePublicApiKey", t("Live API Key")) ?>
     <?= $form->text("almaCheckoutLivePublicApiKey", $almaCheckoutLivePublicApiKey) ?>
 </div>
 
 <div class="form-group">
-    <?= $form->label("almaCheckoutLivePrivateApiKey"), t("Live Private API Key") ?>
+    <?= $form->label("almaCheckoutLivePrivateApiKey", t("Live Private API Key")) ?>
     <?= $form->text("almaCheckoutLivePrivateApiKey", $almaCheckoutLivePrivateApiKey) ?>
 </div>
 
 <?=$form->label('webhook', t('Required Webhook'))?>
 <p>
-    <?= t('Within the Alma Dashboard configure a Webhook endpoint for the following URL'); ?>:
+    <?= t('Within the Alma Dashboard configure a Webhook endpoint for the following URL') ?>:
     <br />
     <a href="<?= Url::to('/checkout/almacheckoutresponse'); ?>">
         <?= Url::to('/checkout/almacheckoutresponse'); ?>
