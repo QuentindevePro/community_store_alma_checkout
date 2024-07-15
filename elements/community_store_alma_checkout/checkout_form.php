1f245b09671900a1196a6932a6ce3bde15967a4f
Quentin DUTILLEUL <quentin.dutilleul@exterieurstock.fr>
2024-07-15 18:00:53 +0200

    Fix du formulaire de checkout

--- elements/community_store_alma_checkout/checkout_form.php
+++ elements/community_store_alma_checkout/checkout_form.php
@@ -1,29 +1,43 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
+<?php
+
+use Alma\API\Entities\Payment;
+
+defined('C5_EXECUTE') or die("Access Denied.");
 extract($vars);
+
+/** @var Payment $payment */
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/@alma/in-page@2.x/dist/index.umd.js"></script>
 <script>
-    $(window).on('load', function() {
-
-        $('.store-btn-complete-order').on('click', function (e) {
-            // Open Checkout with further options
-            var currentpmid = $('input[name="payment-method"]:checked:first').data('payment-method-id');
-
-            if (currentpmid === <?= $pmID; ?>) {
-                $(this).prop('disabled', true);
-                $(this).val('<?= t('Processing...'); ?>');
-
+    $.getScript("https://cdn.jsdelivr.net/npm/@alma/in-page@2.x/dist/index.umd.js").done(() => {
+        $.ajax({
+            url: "<?= \URL::to("/checkout/almacheckoutcreatesession") ?>",
+            type: "GET",
+            cache: false,
+            dataType: 'json',
+            success: (payment) => {
+                console.log("Request successful");
                 const inPage = Alma.InPage.initialize({
-                    merchantId: "TEST MARCHAND",
-                    amountInCents: 20000,
-                    installmentsCount: 3,
-                    selector: "#alma-in-page"
+                    merchantId: payment.merchant_id,
+                    amountInCents: payment.purchase_amount,
+                    installmentsCount: payment.installments_count,
+                    selector: "#alma-in-page",
+                    environment: "<?= $alma_mode ?>",
                 });
 
-                inPage.startPayment({ paymentId: paymentID});
+                document.querySelector("#pay-button").addEventListener("click", () => {
+                    inPage.startPayment({
+                        paymentId: payment.id
+                    });
+                });
             }
+        }).fail((jqXHR, textStatus, errorThrown) => {
+            alert("Error while fetching Alma payment session: " + errorThrown);
         });
 
     });
 </script>
+
+<div id="alma-in-page"></div>
+<button id="pay-button"><?= t("Checkout") ?></button>
